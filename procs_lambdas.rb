e12012492ef8c36016253f87ac86d9dafa60d6b5
Giorgia <giorgia1london@gmail.com>
2014-10-22 14:01:54 +0100

    procs and lambdas

--- procs_lambdas.rb
+++ procs_lambdas.rb
@@ -2,6 +2,22 @@ phrase = Proc.new do |word|
 	p word.upcase.reverse.gsub(/[AEIOU]/, ' lol ')
 end
 
-giorgia = ['vivo', 'sempre', 'insieme', 'ai', 'miei', 'capelli']
+giorgia = ['My', 'name', 'is', 'Giorgia']
 
 p giorgia.each(&phrase)
+
+
+
+def all_together_now_the_beatles(count, chorus, count2, chorus2)
+	count.call
+	chorus.call
+	count2.call
+	chorus2.call
+end
+
+count = Proc.new { [*1..4].each{|num| p num} }
+chorus = Proc.new { puts 'can I have a little more' }
+count2 = Proc.new { [*5..10].each{|num| p num} }
+chorus2 = Proc.new { puts 'I love u' }
+
+all_together_now_the_beatles(count, chorus, count2, chorus2)

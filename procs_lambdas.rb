2a28f719975891a131c081b15e7274cf418ba2d5
Giorgia <giorgia1london@gmail.com>
2014-10-22 17:44:03 +0100

    procs&lambdas

--- procs_lambdas.rb
+++ procs_lambdas.rb
@@ -1,3 +1,5 @@
+# Ex 1
+
 phrase = Proc.new do |word|
 	p word.upcase.reverse.gsub(/[AEIOU]/, ' lol ')
 end
@@ -7,7 +9,9 @@ giorgia = ['My', 'name', 'is', 'Giorgia']
 p giorgia.each(&phrase)
 
 
+#------------------------------------------------------------------#
 
+# Ex 2
 def all_together_now_the_beatles(count, chorus, count2, chorus2)
 	count.call
 	chorus.call
@@ -21,3 +25,87 @@ count2 = Proc.new { [*5..10].each{|num| p num} }
 chorus2 = Proc.new { puts 'I love u' }
 
 all_together_now_the_beatles(count, chorus, count2, chorus2)
+
+
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+#Ex 3
+say_hello_proc = Proc.new { puts 'Hello' }
+say_hello_lambda = lambda { puts 'Hello' }
+
+
+say_hello_proc.call
+say_hello_lambda.call
+
+#-----------------------------------------------------
+
+#Ex 4: who wins in those cases between Batman and Superman?
+
+
+def round1
+	return 'Batman'
+	return 'Superman'
+end
+
+#in the case above the output will be 'Batman' cos the function stops to be executed
+#after the first return. The second return (i.e.return 'Superman')
+#is also called dead code because it will never be executed
+
+#-----------------------------------------------------
+
+#Ex 5
+
+def round2
+	lambda { return 'Batman' }.call
+	return 'Superman'
+end
+
+#in this case  the winner will be Superman because the return is contained in the lambda
+
+#-----------------------------------------------------
+
+#Ex 6
+
+def round3
+	Proc.new { return 'Batman' }.call
+	return 'Superman'
+end
+
+#in this case the winner will be Batman and the second return will never be executed
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
